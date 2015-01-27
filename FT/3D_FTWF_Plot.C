5ac6b3525cd7685cf8120f3a7277e7fabf4c3d05
jgruszko <jgruszko@uw.edu>
2015-01-27 11:32:17 -0800

    Added script that makes an "FT Heat Map" plot, and adjustments to the FT waveform browsing tool.

---
+++ FT/3D_FTWF_Plot.C
@@ -0,0 +1,197 @@
+{
+   gROOT->ProcessLine("#include<complex>");
+   int startRun = 45003831;
+   int endRun = 45003846;
+   char ftFileName[300], gatFileName[300], builtFileName[300], histFileName[300];
+
+   sprintf(ftFileName, "/global/project/projectdirs/majorana/users/jgruszko/FT/FTWF/%d_to_%d", startRun, endRun);
+   TFile *ftFile = new TFile(ftFileName);
+   TTree* FTTree = (TTree*)ftFile->Get("FTTree");
+cout<<"Opened FT file"<<endl;
+   GATDataSet *data = new GATDataSet(startRun, endRun);
+   //TChain* builtChain = data->GetBuiltChain(false);
+   TChain* gatChain = data->GetGatifiedChain(false);
+   if(/*builtChain == NULL ||*/ gatChain == NULL){ break; } 
+   cout<<"Opened gat chain"<<endl;
+   
+   //TFile *builtFile = new TFile("$MJDDATADIR/surfprot/data/built/P3END/OR_run45001822.root");
+   //TTree* MGTree = (TTree*)builtFile->Get("MGTree");
+   
+   //TFile *gatFile = new TFile("$MJDDATADIR/surfprot/data/gatified/P3END/mjd_run45001822.root");
+   //TTree* mjdTree = (TTree*)gatFile->Get("mjdTree");
+   
+   sprintf(histFileName, "/global/project/projectdirs/majorana/users/jgruszko/FT/FT_Power/%d_to_%d.root", startRun, endRun); 
+   TFile *outFile; 
+cout<<"Opened output file"<<endl;
+
+   int nChannels= 18;
+   char wavename[200], gifFile[300], histName[200], canName[200], averageName[200], averageTitle[300], integratedTitle[300], title[300], selection[300], axisTitle[200];
+   TClonesArray* ftArr = new TClonesArray("MGTWaveformFT");
+   std::complex<double> value;
+   double power;
+   vector<double>* energy;
+   vector<int>* timestamp;
+   vector<size_t>* ch;
+   
+   FTTree->SetBranchAddress("FTWaveArr", &ftArr);
+   gatChain->SetBranchAddress("timestamp", &timestamp);
+   gatChain->SetBranchAddress("channel", &ch);
+   gatChain->SetBranchAddress("energyCal", &energy);   
+cout<<"Set branch addresses"<<endl;
+   double energy_keV; 
+   int time;
+   int nentriesFT = FTTree->GetEntries();
+   int nentriesGAT = gatChain->GetEntries();
+   if(nentriesFT < nentriesGAT){ int nentries = nentriesFT ;}
+   else{ int nentries = nentriesGAT; }
+cout<<"Set nentries"<<endl;
+   nentries = 100;
+   int cheasyArr[nChannels] = {112, 113, 114, 115, 118, 119, 120, 121, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153};
+   int cheasy = 0;
+   size_t chan = 0;
+   size_t n = 0;
+   int nWF[nChannels]; //wf counter
+   TH2D* powerHist[nChannels];
+   TH1D* averagePower[nChannels];
+   TH1D* integratedPower[nChannels];
+   TCanvas* canArr[nChannels];
+   MGTWaveformFT* ftWave;
+   //intitialize everything for the FT waveform samples
+   FTTree->GetEntry(0);
+   gatChain->GetEntry(0);
+cout<<"Got entry 0 for intialization."<<endl;
+   if( ftArr->At(0)!= 0){ ftWave = (MGTWaveformFT*) ftArr->At(0); cout<<"Got ftwf 0, 0"<<endl; } 
+   size_t dataLength = ftWave->GetDataLength();
+   double nyquistFreq = ((ftWave->GetSamplingFrequency()/CLHEP::MHz)*0.5);
+   double binWidth = nyquistFreq/dataLength;
+   double startFreq = 0-binWidth/2.0;
+   double endFreq = nyquistFreq - binWidth/2.0;
+   int entriesPass[nChannels];
+cout<<"Set frequency scale"<<endl;
+   //intialize channel plots
+   for(int a = 0; a < nChannels; a++)
+   {
+	nWF[a] = 0;
+	sprintf(histName, "Ch_%d",cheasyArr[a]);
+	sprintf(canName, "can%d", a);
+	sprintf(averageName, "Ch_%d_Average", cheasyArr[a]);
+	sprintf(title, "FT Power vs. Event Number in Channel %d", cheasyArr[a]);
+	sprintf(averageTitle, "FT Power Average in Channel %d", cheasyArr[a]);
+	sprintf(integratedTitle, "Integrated FT Power in Channel %d", cheasyArr[a]);
+	sprintf(selection, "timestamp > 4E10 && channel == %d", cheasyArr[a]);
+	cout<<title<<endl;
+	cout<<averageTitle<<endl;
+	cout<<integratedTitle<<endl;
+	cout<<selection<<endl;
+
+	//use the number of entries that pass the cut
+	entriesPass[a] = (int) gatChain->Draw("channel", selection, "goff");
+	cout<<"Got the number of entries passing the cut."<<endl; 
+	powerHist[a] = new TH2D(histName, title, entriesPass[a], -0.5, entriesPass[a]-0.5, dataLength,startFreq, endFreq);
+	averagePower[a] = new TH1D();
+	averagePower[a]->SetTitle(averageTitle);
+	integratedPower[a] = new TH1D();
+	integratedPower[a]->SetTitle(integratedTitle);
+	canArr[a] = new TCanvas(canName, title, 1000, 1000);
+	canArr[a]->Divide(2,2);
+    }
+cout<<"Initialized everything ok."<<endl;
+
+   for(int i = 0; i < nentries; i++)
+   {
+	if(i%10==0){ cout <<" Checking entry "<< i <<" of "<< nentries<<" i.e. "<<100*(i/nentries)<< " % finished"<<endl; }
+	FTTree->GetEntry(i);
+	//gatChain->GetEntry(i);
+	gatChain->GetEntry(i);
+	n = ch->size();
+	for(int j = 0; j < n; j++)
+	{
+	    chan = ch->at(j); 
+	    time = timestamp->at(j); 
+   	    if( ftArr->At(0)!= 0)
+	    { 
+		ftWave = (MGTWaveformFT*) ftArr->At(j);
+		  
+	    }
+	    else
+	    { 
+		cout<<"Found 0 FT Waveform at event " <<i <<", waveform "<<j<<endl;
+		break; 
+	     }    
+	dataLength = ftWave->GetDataLength();
+	    if(chan == 112) cheasy = 0;
+	    if(chan == 113) cheasy = 1;
+	    if(chan == 114) cheasy = 2;
+	    if(chan == 115) cheasy = 3;
+	    if(chan == 118) cheasy = 4;
+	    if(chan == 119) cheasy = 5;
+	    if(chan == 120) cheasy = 6;
+	    if(chan == 121) cheasy = 7;
+	    if(chan == 144) cheasy = 8;
+	    if(chan == 145) cheasy = 9;
+	    if(chan == 146) cheasy = 10;
+	    if(chan == 147) cheasy = 11;
+	    if(chan == 148) cheasy = 12;
+	    if(chan == 149) cheasy = 13;
+	    if(chan == 150) cheasy = 14;
+	    if(chan == 151) cheasy = 15;
+	    if(chan == 152) cheasy = 16;
+	    if(chan == 153) cheasy = 17;
+cout<<"Got an event ok."<<endl;
+	    if(cheasy > -1 && cheasy < nChannels && time > 4E0 && ftWave != 0)
+	    {
+		for(int k = 0; k < dataLength; k++)
+	    	{
+		    if(k == 0 || k == dataLength - 1)
+		    { 
+		    	power = std::norm(ftWave->At(k)); 
+		    }	
+		    else
+		    { 
+		    	power = 2.*std::norm(ftWave->At(k)); 
+		    }		
+		   	powerHist[cheasy]->SetBinContent(nWF[cheasy], k, power);
+	        }
+		nWF[cheasy]++;
+cout<<"Looped through FT ok"<<endl;
+	     }
+	  }
+	  ftArr->Clear("C");
+cout<<"Cleared ftArr."<<endl;
+	}
+	for(int b=0; b<nChannels; b++)
+	{
+	    //powerHist[b]->GetXaxis()->SetRange(0,nWF[b]);
+	    sprintf(axisTitle, "Event Number in Channel %d (timestamp > 4E10)", cheasyArr[b]);
+	    powerHist[b]->GetXaxis()->SetTitle(axisTitle);
+	    powerHist[b]->GetYaxis()->SetTitle("Frequency (MHz)");
+	    canArr[b]->cd(1);
+	    gPad->SetLogy();
+	    gPad->SetLogz();
+	    canArr[b]->Update();
+	    powerHist[b]->Draw("COLZ");
+	    canArr[b]->cd(2);
+	    gPad->SetLogy();
+	    canArr[b]->Update();
+	    averagePower[b]=powerHist[b]->ProjectionY();
+	    averagePower[b]->Scale(entriesPass[b]);
+	    averagePower[b]->GetXaxis()->SetTitle("Frequency (MHz)");
+	    averagePower[b]->GetYaxis()->SetTitle("Power");
+	    averagePower[b]->Draw();
+	    canArr[b]->cd(4);
+	    canArr[b]->Update();
+	    integratedPower[b]=powerHist[b]->ProjectionX();
+	    integratedPower[b]->GetXaxis()->SetTitle(axisTitle);
+	    integratedPower[b]->GetYaxis()->SetTitle("Integrated Power");
+	    integratedPower[b]->Draw();
+	    
+	    sprintf(gifFile, "/global/project/projectdirs/majorana/users/jgruszko/FT/Plots/3D_Power/%d_to_%d_Ch%d.gif", startRun, endRun, b);
+	    canArr[b]->Print(gifFile);
+
+	    
+   	    outFile= new TFile(histFileName, "UPDATE");
+	    powerHist[b]->Write();
+	    outFile->Close(); 
+	}
+
+}
--- FT/FTWF_Browser.C
+++ FT/FTWF_Browser.C
@@ -1,43 +1,52 @@
 {
    gROOT->Reset();
  
-   TFile *ftFile = new TFile("/global/u1/j/jgruszko/software/FT_Results/FTWF/45000459_to_45000460");
+   TFile *ftFile = new TFile("/global/project/projectdirs/majorana/users/jgruszko/FT/FTWF/45003831_to_45003846");
    TTree* FTTree = (TTree*)ftFile->Get("FTTree");
+   GATDataSet* data = new GATDataSet(45003831, 45003846);
+   TChain* gat = data->GetGatifiedChain(false);
+   TChain* built = data->GetBuiltChain(false);
 
-   FTTree->AddFriend("MGTree", "$MJDDATADIR/surfprot/data/built/P3END/OR_run45000459.root");
-   FTTree->AddFriend("mjdTree","$MJDDATADIR/surfprot/data/gatified/P3END/mjd_run45000459.root");
-   
 
    gROOT->cd();
  
    int nChannels= 18;
-   char wavename[200], ftname[300];
+   char wavename[200], ftname[300], energyText[400], HFText[400], LFText[400];
    char response;
    //TH1D* hFTWFArr[nChannels];
    TClonesArray* ftArr = new TClonesArray("MGTWaveformFT");
    MGTEvent* event = new MGTEvent();
    vector<size_t>* channel;
    vector<double>* energy;
+   vector<double>* lowFreqPower;
+   vector<double>* highFreqPower;
    
    TCanvas* can= new TCanvas("can", "Fourier Transform Browser", 1000, 800);
    can->Divide(2, 2);
-
+   can->cd(3);
+   TPaveText *pt = new TPaveText(.05, .1, .95, .8, "NB");
+ 
    FTTree->SetBranchAddress("FTWaveArr", &ftArr);
-   FTTree->SetBranchAddress("event", &event);
-   FTTree->SetBranchAddress("channel", &channel);
-   FTTree->SetBranchAddress("energyCal", &energy);   
+   built->SetBranchAddress("event", &event);
+   gat->SetBranchAddress("channel", &channel);
+   gat->SetBranchAddress("energyCal", &energy);   
+   FTTree->SetBranchAddress("highFreqPower", &highFreqPower);   
+   FTTree->SetBranchAddress("lowFreqPower", &lowFreqPower);   
 
    MGTWaveformFT* ftWave;
    MGTWaveform* wave;
    MGTWaveform baseline;
    double energy_keV; 
+   double lowPower; 
+   double highPower; 
 
    int nentries = FTTree->GetEntries();
-   nentries = 100;
+   //nentries = 100;
    int cheasy = 0;
    size_t ch = 0;
    size_t n = 0;
-   int jumpTo;
+   int jumpToEvent;
+   int jumpToWF = -1;
 
    TH1D* ftHist = new TH1D();
    TH1D* waveHist = new TH1D(); 
@@ -47,11 +56,17 @@
    {
      if (response == 'q'){  break;  }
      FTTree->GetEntry(i);
+     gat->GetEntry(i);
+     built->GetEntry(i);
      n = channel->size();  
      for(int j=0; j<n; j++)
      {
 	if (response == 'q'){  break;  }
-	
+	if(jumpToWF != -1)
+	{
+	    if(jumpToWF < n) { j = jumpToWF; }
+	    else{ cout << "There is no waveform " << jumpToWF << " in event "<< i <<". Showing waveform "<< j <<" instead." <<endl; }
+	}
 	ch =  channel->at(j);
 	//if(ch == 88) cheasy = 0;
 	//if(ch == 89) cheasy = 1;
@@ -65,25 +80,36 @@
 	if(ch == 152) cheasy = 6;
 	if(ch == 153) cheasy = 7;      
 */
-	if(ch == 150) cheasy =0;
-	if(ch == 151) cheasy =1;                                                                           
-	if (ch==150 || ch==151)
-	{
-	    can->cd(2);
+	can->cd(2);
 	    wave = event->GetWaveform(j);
 	    baseline = *wave;
-	    baseline.SetLength(800);
+	    baseline.SetLength(512);
 	    baselineHist = baseline.GimmeHist();
-	    sprintf(wavename, "Waveform %d of event %d, in channel %d", j, i, ch);
+	    sprintf(wavename, "Baseline of waveform %d of event %d", j, i);
             cout << wavename << endl;
 	    baselineHist->SetTitle(wavename);
 	    baselineHist->Draw();
 	   
 	    energy_keV = energy->at(j);
+	    lowPower = lowFreqPower->at(j);	   
+	    highPower = highFreqPower->at(j);	   
+		
+	can->cd(3);
+	    pt->Clear();
+	    sprintf(energyText, "Onboard energy is %g keV in channel %d", energy_keV, ch);
+	    sprintf(LFText,"Low frequency power is %E", lowPower);
+	    sprintf(HFText,"High frequency power is %E", highPower);
+	    pt->AddText(energyText);
+	    pt->AddText(LFText);
+	    pt->AddText(HFText);
+	    pt->Draw();
 	    cout << "Onboard energy is " << energy_keV << " keV in channel "<< ch << endl;
+	    cout << "Low frequency power is " << lowPower <<endl;
+	    cout << "High frequency power is " << highPower <<endl;
 		
 	    
 	    can->cd(1);
+	    gPad->SetLogy();
 	    ftWave = (MGTWaveformFT*)ftArr->At(j);
 	    ftHist  = ftWave->GimmeUniqueHist();
 	    ftHist->SetTitle("FT Power");
@@ -101,12 +127,16 @@
             cin >> response;
 	   if (response == 'j')
 	   {
-		cout << "Enter event to jump to: ";
-		cin >> jumpTo;
-		i = jumpTo - 1;
+		cout << "Enter 	event to jump to: ";
+		cin >> jumpToEvent;
+		i = jumpToEvent - 1;
+		cout << "Enter 	waveform of event to jump to: ";
+		cin >> jumpToWF;
+		break;
+		
 	   }
 	   else if (response == 'q'){  break;  }
-	
+	   else{ jumpToWF = -1; }	
 	   can->Clear();
 	   can->Divide(2, 2);
 	}
